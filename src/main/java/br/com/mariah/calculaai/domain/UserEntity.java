8021f533f24d3d1a689e7002ca964446d6290fcd
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2023-01-16 03:33:01 -0300

    Service generator class

--- src/main/java/br/com/mariah/calculaai/domain/UserEntity.java
+++ src/main/java/br/com/mariah/calculaai/domain/UserEntity.java
@@ -6,7 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import java.lang.String;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,26 +21,16 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table (
-	name = "User" 
-)
-public class UserEntity { 
+@Table(name = "User")
+public class UserEntity {
 
-	@Id
-	@GeneratedValue (
-		strategy = GenerationType.IDENTITY 
-	)
-	@Column (
-		name = "login" 
-	)
-	private String login;
-	@Column (
-		name = "password" 
-	)
-	private String password;
-	@Column (
-		name = "email" 
-	)
-	private String email; 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
 
 }
--- src/main/java/br/com/mariah/restapi/service/generator/ClassGeneratorsService.java
+++ src/main/java/br/com/mariah/restapi/service/generator/ClassGeneratorsService.java
@@ -4,10 +4,8 @@ import br.com.mariah.restapi.model.ModelData;
 import br.com.mariah.restapi.service.storage.StorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.burningwave.core.classes.UnitSourceGenerator;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
